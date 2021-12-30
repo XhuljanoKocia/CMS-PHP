81b9538f08fc8a9616171f5ebbae622a8a27bf37
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2021-12-30 18:00:50 +0100

    Big Commit

---
+++ includes/db.php
@@ -0,0 +1,24 @@
+<?php
+  // This is a more secure way to connect to the database
+  $db['db_host'] = "localhost";
+  $db['db_user'] = "root";
+  $db['db_pass'] = "";
+  $db['db_name'] = "cms";
+
+  foreach($db as $key => $value) {
+    define(strtoupper($key), $value);
+  }
+
+  $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+  // if($connection) {
+  //   echo "We are connected!";
+  // }
+
+  // This is a simple way to connect to the database
+  // $connection = mysqli_connect('localhost','root','','cms');
+
+  // if($connection) {
+  //   echo "We are connected!";
+  // }
+?>
