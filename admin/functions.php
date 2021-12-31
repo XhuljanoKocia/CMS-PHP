d86945956de135fe80ed4b8c166d395b7823ac73
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2021-12-31 15:29:48 +0100

    Inserting Post Data From admin

--- admin/functions.php
+++ admin/functions.php
@@ -49,4 +49,12 @@
       header("Location: categories.php");
     }
   }
+
+  function confirmQuery($result) {
+    global $connection;
+    
+    if(!$result) {
+      die("Query Failed" . mysqli_error($connection));
+    }
+  }
 ?>
