937a290a887e32cf6c6ea73a918a3e34c1d10a7c
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-01-20 14:25:55 +0100

    Finally Updating Posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -3,10 +3,10 @@
     $the_post_id = $_GET['p_id'];
   }
 
-  $query = "SELECT * FROM posts";
-  $select_posts = mysqli_query($connection, $query);
+  $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+  $select_posts_by_id = mysqli_query($connection, $query);
 
-  while($row = mysqli_fetch_assoc($select_posts)) {
+  while($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_id = $row['post_id'];
     $post_author = $row['post_author'];
     $post_title = $row['post_title'];
@@ -18,15 +18,52 @@
     $post_comment_count = $row['post_comment_count'];
     $post_date = $row['post_date'];
   }
+
+  if(isset($_POST['update_post'])){
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if(empty($post_image)){
+      $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+      $select_image = mysqli_query($connection, $query);
+
+      while($row = mysqli_fetch_array($select_image)){
+        $post_image = $row['post_image'];
+      }
+    }
+
+    $query = "UPDATE posts SET ";
+    $query .="post_title = '{$post_title}', ";
+    $query .="post_category_id = '{$post_category_id}', ";
+    $query .="post_date = now(), ";
+    $query .="post_author = '{$post_author}', ";
+    $query .="post_status = '{$post_status}', ";
+    $query .="post_tags = '{$post_tags}', ";
+    $query .="post_content = '{$post_content}', ";
+    $query .="post_image = '{$post_image}' ";
+    $query .="WHERE post_id = {$the_post_id} ";
+
+    $update_post = mysqli_query($connection, $query);
+
+    confirmQuery($update_post);
+  }
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
   <div class="form-group">
     <label for="title">Post Title</label>
-    <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
+    <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
   </div>
   <div class="form-group">
-    <select name="" id="">
+    <select name="post_category" id="post_category">
       <?php
         $query = "SELECT * FROM categories";
         $select_categories = mysqli_query($connection, $query);
@@ -44,14 +81,15 @@
   </div>
   <div class="form-group">
     <label for="title">Post Author</label>
-    <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
+    <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
   </div>
   <div class="form-group">
     <label for="post_status">Post Status</label>
     <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
   </div>
   <div class="form-group">
-    <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+    <img width="100" src="../images/<?php echo $post_image; ?>" alt="image">
+    <input type="file" name="image" class="form-control">
   </div>
   <div class="form-group">
     <label for="post_tags">Post Tags</label>
@@ -62,6 +100,6 @@
     <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?></textarea>
   </div>
   <div class="form-group">
-    <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+    <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
   </div>
 </form>
