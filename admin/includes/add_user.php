3126500f26bf710aa1ad88f4a92488c3bbe938e7
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-01-25 17:42:36 +0100

    Adding users in admin part 2

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,16 +1,17 @@
 <?php
-  if(isset($_POST['create_post'])) {
-    $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
-    $post_category_id = $_POST['post_category'];
-    $post_status = $_POST['post_status'];
-    $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['tmp_name'];
-    $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
-    $post_date = date('d-m-y');
+  if(isset($_POST['create_user'])) {
+    $user_id = $_POST['user_id'];
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
+    // $post_image = $_FILES['image']['name'];
+    // $post_image_temp = $_FILES['image']['tmp_name'];
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    // $post_date = date('d-m-y');
 
-    move_uploaded_file($post_image_temp, "../images/$post_image");
+    // move_uploaded_file($post_image_temp, "../images/$post_image");
 
     $query = "INSERT INTO posts (post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
     $query .= "VALUES ({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_status}')";
@@ -32,19 +33,9 @@
   </div>
   <div class="form-group">
     <select name="user_role" id="user_role">
-      <?php
-        $query = "SELECT * FROM users";
-        $select_users = mysqli_query($connection, $query);
-
-        confirmQuery($select_users);
-    
-        while($row = mysqli_fetch_assoc($select_users)){
-            $user_id = $row['user_id'];
-            $user_role = $row['user_role'];
-
-            echo "<option value='{$user_id}'>{$user_role}</option>";
-        }
-      ?>
+      <option value="subscriber">Select Options</option>
+      <option value="admin">Admin</option>
+      <option value="subscriber">Subscriber</option>
     </select>
   </div>
   <!-- <div class="form-group">
