448a0e7db253c28c7ff61766fcbf235cec0eaab1
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-02-02 17:23:16 +0100

    Updating User Data

--- admin/profile.php
+++ admin/profile.php
@@ -18,6 +18,31 @@
     }
   }
 ?>
+
+<?php
+  if(isset($_POST['edit_user'])) {
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+
+    $query = "UPDATE users SET ";
+    $query .="user_firstname = '{$user_firstname}', ";
+    $query .="user_lastname = '{$user_lastname}', ";
+    $query .="user_role = '{$user_role}', ";
+    $query .="username = '{$username}', ";
+    $query .="user_email = '{$user_email}', ";
+    $query .="user_password = '{$user_password}' ";
+    $query .="WHERE username = '{$username}' ";
+
+    $edit_user_query = mysqli_query($connection, $query);
+
+    confirmQuery($edit_user_query);
+  }
+?>
+
     <div id="wrapper">
         <!-- Navigation -->
 <?php include "includes/admin_navigation.php"; ?>
