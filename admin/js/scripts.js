66bdf94276e49aa0a7b88277795c2966639fbf1f
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-02-08 15:32:22 +0100

    Adding Bulk Options Posts, part 6

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -4,3 +4,17 @@ $(document).ready(function () {
     console.error(error);
   });
 });
+
+$(document).ready(function () {
+  $("#selectAllBoxes").click(function (event) {
+    if (this.checked) {
+      $(".checkBoxes").each(function () {
+        this.checked = true;
+      });
+    } else {
+      $(".checkBoxes").each(function () {
+        this.checked = false;
+      });
+    }
+  });
+});
