ac7ae1e930f71eb1ae5434700a82939ae9512fa3
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-02-11 15:10:49 +0100

    User Page Dropdown Default Value Change

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -62,7 +62,7 @@
   </div>
   <div class="form-group">
     <select name="user_role" id="user_role">
-      <option value="subscriber"><?php echo $user_role; ?></option>
+      <option value="<?php echo $user_role; ?>"><?php echo $user_role; ?></option>
 <?php
   if($user_role == 'admin'){
     echo "<option value='subscriber'>subscriber</option>";
