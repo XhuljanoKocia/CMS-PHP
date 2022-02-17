1ac042cfd249b820a11ff8f413711f222cce58ae
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-02-17 17:23:02 +0100

    Deleting in the same page

--- admin/post_comments.php
+++ admin/post_comments.php
@@ -69,7 +69,7 @@
     echo "<td>{$comment_date}</td>";
     echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
     echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
-    echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
+    echo "<td><a href='post_comments.php?delete=$comment_id&id=" . $_GET['id'] . "'>Delete</a></td>";
     echo "</tr>";
   }
 ?>
@@ -94,7 +94,7 @@
     $the_comment_id = $_GET['delete'];
     $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id}";
     $delete_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
+    header("Location: post_comments.php?id=" . $_GET['id'] ."");
   }
 ?>
 
