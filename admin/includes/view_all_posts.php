b071cf27486cbc7ebf711ae8beaf57d5157e3663
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-02-21 17:22:42 +0100

    Writing the code

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -142,7 +142,7 @@ include("delete_modal.php");
     echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
     // echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
-    echo "<td><a rel='$post_id' href='' class='delete_link'>Delete</a></td>";
+    echo "<td><a rel='$post_id' href='javascript:void(0)' class='delete_link'>Delete</a></td>";
     echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
     echo "</tr>";
   }
@@ -170,7 +170,11 @@ include("delete_modal.php");
   $(document).ready(function () {
     $(".delete_link").on('click', function(){
       var id = $(this).attr("rel");
-      alert(id);
+      var delete_url = "posts.php?delete="+ id +" ";
+
+      $(".modal_delete_link").attr("href", delete_url);
+      $("#myModal").modal('show');
+
     });
   });
 </script>
