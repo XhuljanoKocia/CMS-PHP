f5e6508ae852f58f0d6631d161864e2c3052e7d0
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-02-24 17:20:58 +0100

    Updating categories with prepare statements

--- admin/includes/update_categories.php
+++ admin/includes/update_categories.php
@@ -1,6 +1,6 @@
-                          <form action="" method="post">
-                            <div class="form-group">
-                              <label for="cat-title">Edit Category</label>
+<form action="" method="post">
+  <div class="form-group">
+    <label for="cat-title">Edit Category</label>
 <?php
   if(isset($_GET['edit'])) {
 
@@ -12,23 +12,25 @@
         $cat_id = $row['cat_id'];
         $cat_title = $row['cat_title'];
 ?>
-                            <input value="<?php if(isset($cat_title)) { echo $cat_title; } ?>" class="form-control" type="text" name="cat_title">
+      <input value="<?php if(isset($cat_title)) { echo $cat_title; } ?>" class="form-control" type="text" name="cat_title">
 <?php }} ?>
 <?php
   //Update query
   if(isset($_POST['update_category'])) {
     $the_cat_title = $_POST['cat_title'];
 
-    $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = {$cat_id}";
-    $update_query = mysqli_query($connection, $query);
+    $stmt = mysqli_prepare($connection, "UPDATE categories SET cat_title = ? WHERE cat_id = ?");
+    mysqli_stmt_bind_param($stmt, 'si', $the_cat_title, $cat_id);
+    mysqli_stmt_execute($stmt);
 
-    if(!$update_query) {
+    if(!$stmt) {
       die("QUERY FAILED" . mysqli_error($connection));
     }
+    redirect("categories.php");
   }
 ?>             
-                            </div>
-                            <div class="form-group">
-                              <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
-                            </div>
-                          </form>
+  </div>
+  <div class="form-group">
+    <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
+  </div>
+</form>
